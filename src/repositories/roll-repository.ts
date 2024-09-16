fc3c1c39f4e8bf27de2b055ec7b71b277988f604
desaxes <desaxes24@gmail.com>
2024-09-16 12:07:00 +1000

    Average values, Indexes

--- src/repositories/roll-repository.ts
+++ src/repositories/roll-repository.ts
@@ -5,21 +5,48 @@ export const RollRepository = {
     async getStandartRolls(finalConditions: any, limit: number, offset: number) {
         const rollCounter = (await srollDb.find<newRollType>(finalConditions).toArray()).length
         const rolls = await srollDb.find().sort({ id: -1 }).skip(offset).limit(limit).toArray()
+        let indexes = []
+        let epicIndexes = []
+        let legs = []
+        let prev = 0
+        let epicPrev = 0
+        let counter = 0
+        for (let i = 0; i < rolls.length; i++) {
+            if (rolls[rolls.length - i - 1].stars === 4) {
+                epicIndexes.push(i + 1 - epicPrev)
+                epicPrev = i + 1
+            }
+            if (rolls[rolls.length - i - 1].stars === 5) {
+                indexes.push(i + 1 - prev)
+                legs.push({
+                    roll: i + 1 - prev,
+                    img: rolls[rolls.length - i - 1].img,
+                    isChar: rolls[rolls.length - i - 1].isChar
+                })
+                prev = i + 1
+                counter++
+                epicIndexes.push(i + 1 - epicPrev)
+                epicPrev = i + 1
+            }
+        }
         return {
             rolls: rolls,
-            total: rollCounter
+            total: rollCounter,
+            middle: Math.floor(indexes.reduce((a, b) => a + b, 0) / counter),
+            epicMiddle:Math.floor(epicIndexes.reduce((a, b) => a + b, 0) / epicIndexes.length),
+            legs: legs
         }
     },
     async getStandartRollStatistic() {
-        const rolls = await srollDb.aggregate([{ $group: { _id: { img: "$img", stars: "$stars", isChar: "$isChar" }, count: { $sum: 1 } } },{ $sort: { '_id.stars': -1 } }]).toArray()
+        const rolls = await srollDb.aggregate([{ $group: { _id: { img: "$img", stars: "$stars", isChar: "$isChar" }, count: { $sum: 1 } } }, { $sort: { '_id.stars': -1, '_id.isChar': -1 } }]).toArray()
         return rolls
     },
     async getEventRollStatistic() {
-        const rolls = await erollDb.aggregate([{ $group: { _id: { img: "$img", stars: "$stars", isChar: "$isChar" }, count: { $sum: 1 } } },{ $sort: { '_id.stars': -1 } }]).toArray()
+        const rolls = await erollDb.aggregate([{ $group: { _id: { img: "$img", stars: "$stars", isChar: "$isChar" }, count: { $sum: 1 } } }, { $sort: { '_id.stars': -1, '_id.isChar': -1 } }]).toArray()
         return rolls
     },
     async getWeaponRollStatistic() {
-        const rolls = await wrollDb.aggregate([{ $group: { _id: { img: "$img", stars: "$stars", isChar: "$isChar" }, count: { $sum: 1 } } },{ $sort: { '_id.stars': -1 } }]).toArray()
+        const rolls = await wrollDb.aggregate([{ $group: { _id: { img: "$img", stars: "$stars", isChar: "$isChar" }, count: { $sum: 1 } } }, { $sort: { '_id.stars': -1, '_id.isChar': -1 } }]).toArray()
         return rolls
     },
     async addStandartRoll(data: rollType) {
@@ -41,9 +68,36 @@ export const RollRepository = {
     async getEventRolls(finalConditions: any, limit: number, offset: number) {
         const rollCounter = (await erollDb.find<newRollType>(finalConditions).toArray()).length
         const rolls = await erollDb.find().sort({ id: -1 }).skip(offset).limit(limit).toArray()
+        let indexes = []
+        let epicIndexes = []
+        let legs = []
+        let prev = 0
+        let epicPrev = 0
+        let counter = 0
+        for (let i = 0; i < rolls.length; i++) {
+            if (rolls[rolls.length - i - 1].stars === 4) {
+                epicIndexes.push(i + 1 - epicPrev)
+                epicPrev = i + 1
+            }
+            if (rolls[rolls.length - i - 1].stars === 5) {
+                indexes.push(i + 1 - prev)
+                legs.push({
+                    roll: i + 1 - prev,
+                    img: rolls[rolls.length - i - 1].img,
+                    isChar: rolls[rolls.length - i - 1].isChar
+                })
+                prev = i + 1
+                counter++
+                epicIndexes.push(i + 1 - epicPrev)
+                epicPrev = i + 1
+            }
+        }
         return {
             rolls: rolls,
-            total: rollCounter
+            total: rollCounter,
+            middle: Math.floor(indexes.reduce((a, b) => a + b, 0) / counter),
+            epicMiddle:Math.floor(epicIndexes.reduce((a, b) => a + b, 0) / epicIndexes.length),
+            legs: legs
         }
     },
     async addEventRoll(data: rollType) {
@@ -65,9 +119,36 @@ export const RollRepository = {
     async getWeaponRolls(finalConditions: any, limit: number, offset: number) {
         const rollCounter = (await wrollDb.find<newRollType>(finalConditions).toArray()).length
         const rolls = await wrollDb.find().sort({ id: -1 }).skip(offset).limit(limit).toArray()
+        let indexes = []
+        let epicIndexes = []
+        let legs = []
+        let prev = 0
+        let epicPrev = 0
+        let counter = 0
+        for (let i = 0; i < rolls.length; i++) {
+            if (rolls[rolls.length - i - 1].stars === 4) {
+                epicIndexes.push(i + 1 - epicPrev)
+                epicPrev = i + 1
+            }
+            if (rolls[rolls.length - i - 1].stars === 5) {
+                indexes.push(i + 1 - prev)
+                legs.push({
+                    roll: i + 1 - prev,
+                    img: rolls[rolls.length - i - 1].img,
+                    isChar: rolls[rolls.length - i - 1].isChar
+                })
+                prev = i + 1
+                counter++
+                epicIndexes.push(i + 1 - epicPrev)
+                epicPrev = i + 1
+            }
+        }
         return {
             rolls: rolls,
-            total: rollCounter
+            total: rollCounter,
+            middle: Math.floor(indexes.reduce((a, b) => a + b, 0) / counter),
+            epicMiddle:Math.floor(epicIndexes.reduce((a, b) => a + b, 0) / epicIndexes.length),
+            legs: legs
         }
     },
     async addWeaponRoll(data: rollType) {
